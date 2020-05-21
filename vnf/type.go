cbb313eda4dbee4f6b1f33a51e4f0e86452ac1d0
Parham Alvani <parham.alvani@gmail.com>
2020-05-21 13:04:26 +0430

    feat: Remove Redundant Functions

--- vnf/type.go
+++ vnf/type.go
@@ -2,23 +2,15 @@ package vnf
 
 // Type is a Virtual Network Function
 type Type struct {
-	Name     string
-	Capacity int
+	Name string
 }
 
-var types = []Type{
-	{"vIDS", 250},
-	{"vNAT", 500},
-	{"vFW", 500},
-	{"vDPI", 500},
-}
-
-// Len return total number of VNFs
-func Len() int {
-	return len(types)
-}
-
-// Get return VNF type that is associated with given index
-func Get(i int) Type {
-	return types[i]
+// Types returns all available VNF types.
+func Types() []Type {
+	return []Type{
+		{"vIDS"},
+		{"vNAT"},
+		{"vFW"},
+		{"vDPI"},
+	}
 }
